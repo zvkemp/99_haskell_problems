b4b9a1d6e350c05593435e6f47e7f0c0b82d5c94
zvkemp <zvkemp@gmail.com>
2014-04-21 16:44:16 -0700

    problem 15

--- problems_11-20.hs
+++ problems_11-20.hs
@@ -6,3 +6,9 @@
 dupli :: [a] -> [a]
 dupli []     = []
 dupli (x:xs) = x : x : dupli xs
+
+--15
+repli :: [a] -> Int -> [a]
+repli [] _     = []
+repli (x:xs) n = (take n (repeat x)) ++ repli xs n
+
