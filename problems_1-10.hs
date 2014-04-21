65f46289437c1f21eb671ca9381c844bda214b2c
zvkemp <zvkemp@gmail.com>
2014-04-21 15:14:40 -0700

    problems 2, 3

--- problems_1-10.hs
+++ problems_1-10.hs
@@ -1,4 +1,20 @@
+--1
 myLast :: [a] -> a
 myLast []     = error "empty list"
 myLast [x]    = x
 myLast (x:xs) = myLast xs
+
+--2
+myButLast :: [a] -> a
+myButLast []     = error "empty list"
+myButLast [x]    = error "only one element"
+myButLast [x, y] = x
+myButLast (x:xs) = myButLast xs
+
+--3
+elementAt :: [a] -> Int -> a
+elementAt [] _ = error "empty list or list too small"
+elementAt (x:xs) n
+  | n == 1 = x
+  | otherwise = elementAt xs (n - 1)
+
