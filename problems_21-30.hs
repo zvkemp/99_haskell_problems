e56fd73b8cd4d26e61e81a964109c7e953e643ec
zvkemp <zvkemp@gmail.com>
2014-04-21 21:26:29 -0700

    problem 22

--- problems_21-30.hs
+++ problems_21-30.hs
@@ -5,3 +5,16 @@ insertAt a xs index = front ++ middle ++ back
   where front  = take (index - 1) xs
         middle = [a] 
         back   = drop (index - 1) xs 
+
+
+--22
+range :: (Integral a) => a -> a -> [a]
+range x y = [x..y]
+
+
+--23
+--24
+--TODO
+
+
+
